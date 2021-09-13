6855d4d61e735aabb2a22974078ce5f164b7705e
Azeibardzan <90623342+Azeibardzan@users.noreply.github.com>
2021-09-13 16:06:55 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { Button } from "@material-ui/core";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Button color="primary">Hello World</Button>;
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
